2fe4378f8214a78bb818ff13be485789992cfbd6
Pablo Matias Gomez <pablomatiasgomez@gmail.com>
2020-12-28 20:12:41 -0300

    add support for zonaprop list

--- src/connector/browsers/zonaprop-browser.js
+++ src/connector/browsers/zonaprop-browser.js
@@ -4,9 +4,20 @@ const logger = include('utils/logger').newLogger('ZonaPropBrowser');
 
 //---------------
 
-const URL_REGEX = /^https?:\/\/www.zonaprop.com.ar\/.*-(\d+).html$/;
+const LISTING_URL_REGEX = /^https?:\/\/www.zonaprop.com.ar\/propiedades\/.*-(\d+).html$/;
+const LISTINGS_URL_REGEX = /^https?:\/\/www.zonaprop.com.ar\/([\w-]*).html$/;
 
 function ZonaPropBrowser() {
+    this.extractDataFns = [
+        {
+            regex: LISTING_URL_REGEX,
+            fn: this.extractListingData,
+        },
+        {
+            regex: LISTINGS_URL_REGEX,
+            fn: this.extractListData,
+        },
+    ];
 }
 
 ZonaPropBrowser.prototype.useStealthBrowser = function () {
@@ -18,17 +29,28 @@ ZonaPropBrowser.prototype.name = function () {
 };
 
 ZonaPropBrowser.prototype.acceptsUrl = function (url) {
-    return URL_REGEX.test(url);
+    return this.extractDataFns.some(entry => entry.regex.test(url));
 };
 
 ZonaPropBrowser.prototype.getId = function (url) {
-    let match = URL_REGEX.exec(url);
-    if (!match || match.length !== 2) throw "Url couldn't be parsed: " + url;
-    return match[1];
+    return this.extractDataFns
+        .map(entry => entry.regex.exec(url))
+        .filter(match => match && match.length === 2)
+        .map(match => match[1])
+        [0];
 };
 
 ZonaPropBrowser.prototype.extractData = function (browserPage) {
-    logger.info(`Extracting data...`);
+    let self = this;
+
+    return self.extractDataFns
+        .filter(entry => entry.regex.test(browserPage.url()))
+        .map(entry => entry.fn.call(self, browserPage))
+        [0];
+};
+
+ZonaPropBrowser.prototype.extractListingData = function (browserPage) {
+    logger.info(`Extracting listing data...`);
 
     return browserPage.evaluate(() => {
         let response = {
@@ -45,6 +67,22 @@ ZonaPropBrowser.prototype.extractData = function (browserPage) {
         return response;
     });
 };
+
+ZonaPropBrowser.prototype.extractListData = function (browserPage) {
+    logger.info(`Extracting list data...`);
+
+    return browserPage.evaluate(() => {
+        let response = {
+            EXPORT_VERSION: "0"
+        };
+
+        // noinspection JSUnresolvedVariable,JSHint
+        Object.assign(response, JSON.parse(JSON.stringify(postingInfo)));
+
+        return response;
+    });
+};
+
 
 // ---------
 
