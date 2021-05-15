c797d75f1325698c498bf75daea5d303e5f17d55
Pablo Matias Gomez <pablomatiasgomez@gmail.com>
2021-05-14 23:04:39 -0300

    fix meli new view

--- src/connector/browsers/mercadolibre-browser.js
+++ src/connector/browsers/mercadolibre-browser.js
@@ -46,6 +46,7 @@ MercadoLibreBrowser.prototype.extractData = function (browserPage) {
 
             let gaScript = findScript("dimension120");
             let seller = /meli_ga\("set", "dimension120", "(.*)"\)/.exec(gaScript)[1];
+            // TODO this could be removed if other versions removed.
             seller = seller.toLowerCase().split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
 
             let features = {};
@@ -54,6 +55,7 @@ MercadoLibreBrowser.prototype.extractData = function (browserPage) {
                 features[keyValue[0]] = keyValue[1] || true;
             });
 
+            // TODO item key could be removed if other versions removed.
             let itemKey = /\.com\/(.*)-D_NQ/.exec(JSON.parse(document.querySelector("script[type='application/ld+json']").innerText).image)[1];
             let pictureUrlsScript = findScript("items = [");
             let pictureUrls = JSON.parse(/items = (.*),\n/.exec(pictureUrlsScript)[1])
@@ -104,14 +106,52 @@ MercadoLibreBrowser.prototype.extractData = function (browserPage) {
                 features: features,
                 pictureUrls: pictureUrls,
             };
-        } else {
-            throw document.getElementsByTagName("html")[0].innerHTML;
-            // If none of the two versions are found, it means we were redirected to the home page..
+        } else if (document.querySelector(".ui-pdp-title")) {
+            // Legacy, but updated version of MELI listings....
+
+            let statusEl = document.querySelector(".ui-pdp-container--pdp .ui-pdp-message");
+            let status = statusEl ? statusEl.innerText.trim() : "ONLINE";
+
+            let title = document.querySelector(".ui-pdp-container--pdp .ui-pdp-title").innerText.trim();
+            let description = document.querySelector(".ui-pdp-container--pdp .ui-pdp-description__content").innerText.split(/(?:\n|\. )+/).map(l => l.trim()).filter(l => !!l);
+            let price = document.querySelector(".ui-pdp-container--pdp .price-tag-amount").innerText.replace("\n", " ").trim();
+            let address = document.querySelector(".ui-pdp-container--pdp .ui-vip-location__subtitle").innerText.replace(", Capital Federal, Capital Federal", ", Capital Federal").trim();
+
+            // TODO Remove this replacements once legacy view is removed..
+            let seller = document.querySelector(".ui-vip-profile-info h3").innerText.replace(" ", "").trim();
+            seller = seller.charAt(0).toUpperCase() + seller.slice(1).toLowerCase();
+
+            let features = [...document.querySelectorAll(".ui-pdp-container--pdp .ui-pdp-specs__table table tr")].reduce((features, tr) => {
+                features[tr.querySelector("th").innerText.trim()] = tr.querySelector("td").innerText.trim();
+                return features;
+            }, {});
+
+            // TODO item key could be removed when legacy vervion removed..
+            let itemKey = /MLA-\d+-(.*)_JM/.exec(document.querySelector("meta[property='og:url']").getAttribute('content'))[1];
+            let pictureUrls = [...document.querySelectorAll(".ui-pdp-container--pdp .ui-pdp-gallery .ui-pdp-gallery__column img.ui-pdp-image.ui-pdp-gallery__figure__image")]
+                .map(i => i.getAttribute("data-zoom"))
+                .map(url => url.replace("mlstatic.com/D", `mlstatic.com/${itemKey}D`));
+
+            return {
+                EXPORT_VERSION: EXPORT_VERSION,
+                status: status,
+                title: title,
+                description: description,
+                price: price,
+                address: address,
+                seller: seller,
+                features: features,
+                pictureUrls: pictureUrls,
+            };
+        } else if (document.querySelector(".ui-search-main")) {
+            // Redirected to search view.
             // No data was found (probably got redirected and the house no longer exists)
             return {
                 EXPORT_VERSION: EXPORT_VERSION,
                 status: "OFFLINE",
             };
+        } else {
+            throw "Couldn't find any valid element! HTML: \n " + document.getElementsByTagName("html")[0].innerHTML;
         }
     });
 };
