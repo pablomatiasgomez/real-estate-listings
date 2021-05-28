f663183a6fdbd685e61e30e133d7f3f1cda09ba2
Pablo Matias Gomez <pablomatiasgomez@gmail.com>
2021-05-28 17:40:01 -0300

    Fix Varcasia

--- src/connector/browsers/varcasia-browser.js
+++ src/connector/browsers/varcasia-browser.js
@@ -24,15 +24,15 @@ VarcasiaBrowser.prototype.extractData = function (browserPage) {
     return browserPage.evaluate(() => {
         let EXPORT_VERSION = "1";
 
-        let titleEl = document.querySelector(".mh-top-title__heading");
-        if (!titleEl) {
+        if (document.querySelector(".mh-404")) {
+            // Got not found page, property unlisted
             return {
                 EXPORT_VERSION: EXPORT_VERSION,
                 status: "UNLISTED"
             };
         }
 
-        let title = titleEl.innerText.trim();
+        let title = document.querySelector(".mh-top-title__heading").innerText.trim();
         let price = document.querySelector(".mh-estate__details__price__single").innerText;
         let features = {};
         [...document.querySelectorAll(".mh-estate__list li")].map(li => {
