bc182ba2f220567e10f7c81ea15ea5946d6f0f22
Pablo Matias Gomez <pablomatiasgomez@gmail.com>
2021-06-13 14:47:00 -0300

    argenprop listings fix

--- src/connector/browsers/argenprop-listings-browser.js
+++ src/connector/browsers/argenprop-listings-browser.js
@@ -22,38 +22,37 @@ ArgenPropListingsBrowser.prototype.extractListPage = function (browserPage) {
     logger.info(`Extracting list data for ${browserPage.url()}...`);
 
     return browserPage.evaluate(() => {
-        try {
-            let response = {
-                EXPORT_VERSION: "1"
+        let response = {
+            EXPORT_VERSION: "1"
+        };
+
+        [...document.querySelectorAll(".listing__item")].filter(item => {
+            // Some items do not have link, and other info, so we discard them..
+            return !!item.querySelector("a.card");
+        }).forEach(item => {
+            let url = item.querySelector("a.card").href;
+            let id = url.split("--")[1];
+
+            let price = item.querySelector(".card__price").innerText.trim();
+            let address = item.querySelector(".card__address").innerText.trim();
+            let title = item.querySelector(".card__title").innerText.trim();
+            let features = [...item.querySelectorAll(".card__common-data span")].map(i => i.innerText.trim());
+            let description = item.querySelector(".card__info").innerText.split(/(?:\n|\. )+/).map(l => l.trim()).filter(l => !!l);
+
+            response[id] = {
+                url: url,
+                price: price,
+                address: address,
+                title: title,
+                features: features,
+                description: description
             };
+        });
 
-            [...document.querySelectorAll(".listing__item")].forEach(item => {
-                let url = item.querySelector("a.card").href;
-                let id = url.split("--")[1];
-
-                let price = item.querySelector(".card__price").innerText.trim();
-                let address = item.querySelector(".card__address").innerText.trim();
-                let title = item.querySelector(".card__title").innerText.trim();
-                let features = [...item.querySelectorAll(".card__common-data span")].map(i => i.innerText.trim());
-                let description = item.querySelector(".card__info").innerText.split(/(?:\n|\. )+/).map(l => l.trim()).filter(l => !!l);
-
-                response[id] = {
-                    url: url,
-                    price: price,
-                    address: address,
-                    title: title,
-                    features: features,
-                    description: description
-                };
-            });
-
-            response.pages = [...document.querySelectorAll(".pagination__page:not(.pagination__page-prev):not(.pagination__page-next)")]
-                .map(a => parseInt(a.innerText))
-                .filter(page => !isNaN(page));
-            return response;
-        } catch (e) {
-            throw "Err: " + e + "\n HTML: \n " + document.getElementsByTagName("html")[0].innerHTML;
-        }
+        response.pages = [...document.querySelectorAll(".pagination__page:not(.pagination__page-prev):not(.pagination__page-next)")]
+            .map(a => parseInt(a.innerText))
+            .filter(page => !isNaN(page));
+        return response;
     });
 };
 
