269f811892ddf330a2306a664d544e6c83407b4a
Pablo Matias Gomez <pablomatiasgomez@gmail.com>
2021-08-05 22:45:47 -0300

    Expose saved data

--- src/controller/web-api-controller.js
+++ src/controller/web-api-controller.js
@@ -87,7 +87,7 @@ WebApiController.prototype.listen = function (port) {
                 logger.error(error);
                 return res.status(500).send(error.message);
             } else if (!response) {
-                return res.status(404);
+                return res.status(404).send({});
             } else {
                 return res.send(response);
             }
