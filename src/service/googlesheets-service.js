e44e58332f592cd586e363eb7dcb0590820dcd07
Pablo Matias Gomez <pablomatiasgomez@gmail.com>
2023-11-11 00:26:52 -0300

    fix upgrae of lib

--- src/service/googlesheets-service.js
+++ src/service/googlesheets-service.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const GoogleSpreadsheet = require('google-spreadsheet').GoogleSpreadsheet;
-const GoogleSpreadsheetUtils = require('google-spreadsheet/lib/utils');
+const JWT = require('google-auth-library').JWT;
 
 const logger = newLogger('GoogleSheetsService');
 
@@ -9,12 +9,17 @@ const logger = newLogger('GoogleSheetsService');
 
 class GoogleSheetsService {
     constructor() {
-        this.doc = new GoogleSpreadsheet(config.urlsSource.googleSheet.spreadsheetId);
+        const jwt = new JWT({
+            email: config.urlsSource.googleSheet.credentials.client_email,
+            key: config.urlsSource.googleSheet.credentials.private_key,
+            scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+        });
+        this.doc = new GoogleSpreadsheet(config.urlsSource.googleSheet.spreadsheetId, jwt);
     }
 
     getUrls() {
         let self = this;
-        return self.doc.useServiceAccountAuth(config.urlsSource.googleSheet.credentials).then(() => {
+        return Promise.resolve().then(() => {
             return self.doc.loadInfo();
         }).then(() => {
             let urls = [];
@@ -32,6 +37,7 @@ class GoogleSheetsService {
     }
 
     getUrlsFromSheet(sheet) {
+        let self = this;
         logger.info(`Getting urls from sheet ${sheet.title}`);
         return Promise.resolve().then(() => {
             return sheet.getCellsInRange('A1:Z1');
@@ -41,7 +47,7 @@ class GoogleSheetsService {
             let columnIndex = cells[0].indexOf("links");
             if (columnIndex === -1) return [];
 
-            let columnLetter = GoogleSpreadsheetUtils.columnToLetter(columnIndex + 1);
+            let columnLetter = self.columnToLetter(columnIndex + 1);
             logger.info(`Using column ${columnLetter}`);
 
             return sheet.getCellsInRange(`${columnLetter}2:${columnLetter}1000`);
@@ -52,6 +58,20 @@ class GoogleSheetsService {
                 .map(link => link.trim());
         });
     }
+
+    // Copied from 'google-spreadsheet/lib/utils' as it is not exported.
+    columnToLetter(columnIndex) {
+        let temp;
+        let letter = '';
+        let col = columnIndex;
+        while (col > 0) {
+            temp = (col - 1) % 26;
+            letter = String.fromCharCode(temp + 65) + letter;
+            col = (col - temp - 1) / 26;
+        }
+        return letter;
+    }
+
 }
 
 module.exports = GoogleSheetsService;
