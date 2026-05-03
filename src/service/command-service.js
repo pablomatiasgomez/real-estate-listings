a3ea3bb7a7d19f32161f4259abbbbdda5f9fe223
Pablo Matias Gomez <pablomatiasgomez@gmail.com>
2026-05-03 12:51:22 -0300

    handle command startup failures correctly

--- src/service/command-service.js
+++ src/service/command-service.js
@@ -2,11 +2,11 @@
 
 const spawn = require('child_process').spawn;
 
-const Utils = require('../utils/utils.js');
-
 const logger = newLogger('CommandService');
 
 //----------------------
+
+const STARTUP_WAIT_MS = 6000;
 
 class CommandService {
     constructor(command) {
@@ -19,18 +19,24 @@ class CommandService {
         logger.info(`Running ${this.command} ${this.args.join(" ")}`);
         this.proc = spawn(this.command, this.args);
 
-        // Listen to stdout and stderr
         this.proc.stdout.on('data', (data) => logger.info(`[stdout][${this.command}]: ${data.toString()}`));
         this.proc.stderr.on('data', (data) => logger.error(`[stderr][${this.command}]: ${data.toString()}`));
-
-        // Listen to exit
-        this.proc.on('close', (code) => logger.info(`Command ${this.command} exited with code ${code}`));
-
-        // Handle errors
         this.proc.on('error', (err) => logger.error(`Command ${this.command} failed to start process: ${err.message}`));
 
-        // This is not ideal, but we eed to wait to process to start...
-        return Promise.resolve().then(Utils.delay(3000));
+        return new Promise((resolve, reject) => {
+            let startupComplete = false;
+            this.proc.on('close', (code) => {
+                logger.info(`Command ${this.command} exited with code ${code}`);
+                if (!startupComplete) {
+                    reject(new Error(`Command ${this.command} exited with code ${code} before completing startup`));
+                }
+            });
+            // Give the process a window to fail (e.g. ssh ConnectTimeout). If it stays alive, assume it's up.
+            setTimeout(() => {
+                startupComplete = true;
+                resolve();
+            }, STARTUP_WAIT_MS);
+        });
     }
 
     close() {
